a00438889a062abc7c0f1671b76eec0316500798
ibeastking <omajitpatil@gmail.com>
2023-01-24 14:06:09 +0530

    first commit

---
+++ mul.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class mul {
+    public static void main(String[] args) {
+        int a, b;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number: ");
+        a = sc.nextInt();
+        System.out.println("Enter Number: ");
+        b = sc.nextInt();
+
+        int c = a * b;
+        System.out.println("Number\nA: " + a + "\nB: " + b + "\nC: " + c);
+        sc.close();
+    }
+}
